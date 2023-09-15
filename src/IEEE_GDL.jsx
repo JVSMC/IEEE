f3553e698fc76477b23f8f93af5571dd1c88e851
Lap_Javi-Local <javixxmc@gmail.com>
2023-09-14 20:04:21 -0600

    --| First commit |--

---
+++ src/IEEE_GDL.jsx
@@ -0,0 +1,11 @@
+import GlobalNavBar from "./components/globalNavBar/GlobalNavBar";
+
+function IEEE_GDL() {
+    return ( 
+        <>
+           <GlobalNavBar/>
+        </>
+     );
+}
+
+export default IEEE_GDL;
---
+++ src/components/globalNavBar/GlobalNavBar.jsx
@@ -0,0 +1,15 @@
+import '../globalNavBar/GlobalNavBar.css';
+
+function GlobalNavBar() {
+    return ( 
+        <nav>
+            <ul><a href="https://www.ieee.org/" target='_blank'>IEEE.org</a></ul>
+            <ul><a href="https://ieeexplore.ieee.org/Xplore/home.jsp" target='_blank'>IEEE Xplore Digital Library</a></ul>
+            <ul><a href="https://standards.ieee.org/" target='_blank'>IEEE Standards </a></ul>
+            <ul><a href="https://spectrum.ieee.org/" target='_blank'>IEEE Spectrum </a></ul>
+            <ul><a href="https://www.ieee.org/sitemap.html" target='_blank'>More Sites</a></ul>
+        </nav>
+     );
+}
+
+export default GlobalNavBar;
